bcf2663f745f2a6f12bb23c6d47d914a6d999a31
mtrop <mtropiano@gmail.com>
2026-05-30 17:12:59 -0400

    Add updater to GUI.

--- src/main/java/net/mtrop/doomy/gui/DoomyGUIMainWindow.java
+++ src/main/java/net/mtrop/doomy/gui/DoomyGUIMainWindow.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019-2025 Matt Tropiano
+ * Copyright (c) 2019-2026 Matt Tropiano
  * This program and the accompanying materials are made available under 
  * the terms of the MIT License, which accompanies this distribution.
  ******************************************************************************/
@@ -11,6 +11,7 @@ import javax.swing.JMenuBar;
 
 import net.mtrop.doomy.DoomyCommon;
 import net.mtrop.doomy.DoomyEnvironment;
+import net.mtrop.doomy.DoomyUpdater;
 import net.mtrop.doomy.gui.swing.AboutJavaPanel;
 import net.mtrop.doomy.gui.swing.AboutPanel;
 import net.mtrop.doomy.gui.swing.DoomFetchControlPanel;
@@ -18,11 +19,17 @@ import net.mtrop.doomy.gui.swing.EngineTableControlPanel;
 import net.mtrop.doomy.gui.swing.IdGamesSearchControlPanel;
 import net.mtrop.doomy.gui.swing.IwadTableControlPanel;
 import net.mtrop.doomy.gui.swing.PresetTableControlPanel;
+import net.mtrop.doomy.gui.swing.ProgressPanel;
 import net.mtrop.doomy.gui.swing.SettingsPanel;
 import net.mtrop.doomy.gui.swing.WadTableControlPanel;
 import net.mtrop.doomy.managers.GUIManager;
 import net.mtrop.doomy.managers.LanguageManager;
+import net.mtrop.doomy.managers.LoggerManager;
+import net.mtrop.doomy.managers.TaskManager;
+import net.mtrop.doomy.struct.InstancedFuture;
+import net.mtrop.doomy.struct.LoggingFactory.Logger;
 import net.mtrop.doomy.struct.swing.SwingUtils;
+import net.mtrop.doomy.struct.util.ObjectUtils;
 
 import java.awt.BorderLayout;
 import java.awt.Desktop;
@@ -30,7 +37,9 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import static javax.swing.BorderFactory.createEmptyBorder;
 import static net.mtrop.doomy.struct.swing.ComponentFactory.*;
 import static net.mtrop.doomy.struct.swing.ContainerFactory.*;
 import static net.mtrop.doomy.struct.swing.ModalFactory.*;
@@ -43,6 +52,8 @@ import static net.mtrop.doomy.struct.swing.LayoutFactory.*;
 public class DoomyGUIMainWindow extends JFrame
 {
 	private static final long serialVersionUID = -7341160374312252193L;
+
+	private static final Logger LOG = LoggerManager.getLogger(DoomyGUIMainWindow.class);
 	
 	/** Doomy Version */
 	public static final String VERSION = DoomyCommon.getVersionString("doomy");
@@ -108,7 +119,9 @@ public class DoomyGUIMainWindow extends JFrame
 				gui.createItemFromLanguageKey("menu.help.about", (i) -> onAbout()),
 				gui.createItemFromLanguageKey("menu.help.about.java", (i) -> onAboutJava()),
 				separator(),
-				gui.createItemFromLanguageKey("menu.help.open.config", (i) -> onOpenConfigFolder())
+				gui.createItemFromLanguageKey("menu.help.open.config", (i) -> onOpenConfigFolder()),
+				separator(),
+				gui.createItemFromLanguageKey("menu.help.update", (i) -> onUpdate())
 			)
 		);
 	}
@@ -151,6 +164,133 @@ public class DoomyGUIMainWindow extends JFrame
 		}
 	}
 	
+	private void onUpdate() 
+	{
+		final String path; 
+		try {
+			path = DoomyEnvironment.getDoomyPath();
+		} catch (SecurityException e) {
+			SwingUtils.error(language.getText("doomy.error.pathenvvar"));
+			return;
+		}
+		
+		if (ObjectUtils.isEmpty(path))
+		{
+			SwingUtils.error(language.getText("doomy.error.pathenvvar"));
+			return;
+		}
+		
+		final ProgressPanel progressPanel = new ProgressPanel(48);
+		progressPanel.setActivityMessage("Please wait...");
+		progressPanel.setProgressLabel("");
+		progressPanel.setIndeterminate();
+		
+		Modal<Object> progressModal = modal(
+			gui.getWindowIcons(),
+			language.getText("doomy.update.title"),
+			containerOf(createEmptyBorder(8, 8, 8, 8), node(BorderLayout.CENTER, progressPanel)),
+			gui.createChoiceFromLanguageKey("choice.cancel")
+		);
+
+		final AtomicBoolean successful = new AtomicBoolean(false);
+		
+		// Listener 
+		DoomyUpdater.Listener listener = new DoomyUpdater.Listener() 
+		{
+			@Override
+			public void onMessage(String message) 
+			{
+				progressPanel.setActivityMessage(message);
+			}
+
+			@Override
+			public void onError(String message) 
+			{
+				SwingUtils.error(progressModal, message);
+				progressPanel.setErrorMessage(language.getText("doomy.update.failed"));
+			}
+
+			@Override
+			public void onDownloadStart() 
+			{
+				progressPanel.setActivityMessage(language.getText("doomy.update.downloading"));
+				progressPanel.setProgressLabel("0%");
+			}
+
+			@Override
+			public void onDownloadTransfer(long current, Long max) 
+			{
+				int kbs = (int)(current / 1024L);
+				if (max != null)
+				{
+					int maxkbs = (int)(max / 1024L);
+					int pct = kbs * 100 / maxkbs;
+					progressPanel.setActivityMessage(language.getText("doomy.update.downloading.amount2", kbs, maxkbs));
+					progressPanel.setProgressLabel(pct + "%");
+					progressPanel.setProgress(0, kbs, maxkbs);
+				}
+				else // length was not in response
+				{
+					progressPanel.setActivityMessage(language.getText("doomy.update.downloading.amount1", kbs));
+					progressPanel.setProgressLabel("N/A");
+				}
+			}
+
+			@Override
+			public void onDownloadFinish() 
+			{
+				progressPanel.setActivityMessage(language.getText("doomy.update.downloading.finished"));
+				progressPanel.setProgressLabel("100%");
+				progressPanel.setProgress(0, 100, 100);
+			}
+
+			@Override
+			public boolean shouldContinue(String versionString)
+			{
+				progressPanel.setActivityMessage(language.getText("doomy.update.downloading.found"));
+				return SwingUtils.yesTo(progressModal, language.getText("doomy.update.continue", versionString));
+			}
+
+			@Override
+			public void onUpToDate() 
+			{
+				progressPanel.setSuccessMessage(language.getText("doomy.update.downloading.uptodate"));
+				progressPanel.setProgressLabel("100%");
+				progressPanel.setProgress(0, 100, 100);
+			}
+
+			@Override
+			public void onUpdateSuccessful() 
+			{
+				progressPanel.setSuccessMessage(language.getText("doomy.update.downloading.success"));
+				successful.set(true);
+			}
+
+			@Override
+			public void onUpdateAbort() 
+			{
+				progressPanel.setErrorMessage(language.getText("doomy.update.downloading.aborted"));
+				progressPanel.setProgressLabel("");
+				progressPanel.setProgress(0, 0, 100);
+			}
+		};
+		
+		try {
+			
+			InstancedFuture<Integer> instance = TaskManager.get().spawn(new DoomyUpdater(new File(path), listener));
+			progressModal.openThenDispose(); // will hold here until closed.
+			if (!instance.isDone())
+				instance.cancel();
+			
+			if (successful.get())
+				SwingUtils.info(this, language.getText("doomy.update.success"));
+			
+		} catch (Exception e) {
+			LOG.error(e, "Uncaught error during update.");
+			SwingUtils.error(this, "Uncaught error during update: " + e.getClass().getSimpleName());
+		}
+	}
+
 	private void attemptClose()
 	{
 		if (onCloseAttempt())
